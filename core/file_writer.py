368ec05f48b1e5aefb507eaa279fcd718decd8b6
Aidan Katz <akatz42@gatech.edu>
2025-05-18 15:00:21 -0400

    Add safe file writing and appending

---
+++ core/file_writer.py
@@ -0,0 +1,13 @@
+'''
+Purpose: Safe writing and creation of files.
+
+Responsibilities:
+- Prevent overwrite unless approved
+- Ensure directories exist
+- Apply templates
+
+Spec:
+- write_file(path: str, content: str)
+- create_folder(path: str)
+- backup_existing(path: str)
+'''
