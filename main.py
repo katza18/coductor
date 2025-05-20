76aff059c06c6768c6cbf9cd23f86b65827eac67
Aidan Katz <akatz42@gatech.edu>
2025-05-19 21:31:12 -0400

    Add new commands

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import typer
-from core.commands import build
+from core.commands import build, add, tests
 
 app = typer.Typer()
 app.add_typer(build.app, name="build")
+app.add_typer(add.app, name="add")
+app.add_typer(tests.app, name="tests")
 
 if __name__ == "__main__":
     app()
